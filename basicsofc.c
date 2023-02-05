a4f46fac3766eef81090a947c9431df77745a977
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2023-02-05 17:48:35 +0530

    printing my name using C

--- basicsofc.c
+++ basicsofc.c
@@ -2,5 +2,10 @@
 
 int main()
 {
-   printf("Hello world");//print the hello world
+   printf("Hello world\n");//print the hello world
+   
+   //printing my name
+   
+   printf("Jadamal Sangeetha");
+   return 0;
 }
