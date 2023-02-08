8fcfad2aa83e42f54cc689bbcf157c81efb1d448
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2023-02-08 19:05:20 +0530

    arthimatic operators

---
+++ arthimatic_operators.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int a=10, b=20;
+    printf("sum of two numbers: %f",a+b);
+    printf("\n sub of two numbers: %f",a-b);
+    printf("\n multipliaction of two numbers: %d",a*b);
+    printf("\n division of two numbers: %f",a/b);
+    printf("\n division 2 of two numbers: %f",b/a);
+    //printf("\n modulus of two numbers: %f",a%b); 
+    //printf("\n modulus 2 of two numbers: %f",b%a);
+}
