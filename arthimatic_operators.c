782f09a66113304a53f11c01a54d259a86e21028
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2023-02-09 23:35:11 +0530

    Update arthimatic_operators.c

--- arthimatic_operators.c
+++ arthimatic_operators.c
@@ -3,11 +3,11 @@
 int main()
 {
     int a=10, b=20;
-    printf("sum of two numbers: %f",a+b);
-    printf("\n sub of two numbers: %f",a-b);
+    printf("sum of two numbers: %d",a+b);
+    printf("\n sub of two numbers: %d",a-b);
     printf("\n multipliaction of two numbers: %d",a*b);
-    printf("\n division of two numbers: %f",a/b);
-    printf("\n division 2 of two numbers: %f",b/a);
-    //printf("\n modulus of two numbers: %f",a%b); 
-    //printf("\n modulus 2 of two numbers: %f",b%a);
+    printf("\n division of two numbers: %d",a/b);
+    printf("\n division 2 of two numbers: %d",b/a);
+    printf("\n modulus of two numbers: %d",a%b); 
+    printf("\n modulus 2 of two numbers: %d",b%a);
 }
