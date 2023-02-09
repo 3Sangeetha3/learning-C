3ab5936d66d7e374e444f9c0403168a404ff74d2
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2023-02-09 23:42:27 +0530

    Update if_else.c

--- if_else.c
+++ if_else.c
@@ -5,10 +5,10 @@ int main()
     int a=10, b=20;
     if (a>b)//do not have to add semicolon becoz the statement is not terminated here 
     //if added it will understand that the statement is completed and print the next line i.e, is a is greater than b
-    {printf("a is greater than b");//if alone can bee printed without else
+    {printf("a is greater than b");//if alone can be printed without else
     printf("\nhello");}// we have to create a block using cruly brackets
-    //print nothing becoz a is less and print if a is greater 
-    //in a if condition we have to add parenthesis ()
+    //print nothing becoz a is less and print the statement if a is greater 
+    //In if condition we have to add parenthesis ()
     //else will work iff if is false
     else
         {printf("a is less than b");
