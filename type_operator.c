1119a54a761dab60cd102e3de451d20a78644a18
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2023-02-09 23:26:15 +0530

    Update type_operator.c

--- type_operator.c
+++ type_operator.c
@@ -1,1 +1,19 @@
-#include
+#include <stdio.h>
+int main()
+{
+    //implict type conversion
+    int a=10;
+    float b;
+    b=a;
+    printf("float b=%f",b);
+    printf("\t integer a=%d",a);
+
+    //explict conversion
+    int a=10;
+    float b;
+    b = (float)a;
+    a = (int)b;
+    printf("float b=%f",b);
+    printf("\t integer a=%d",a);
+
+}
