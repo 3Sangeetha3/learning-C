ea1419b0af6eefc7d9c29c94cd0c45919ab0fa61
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2023-02-18 21:14:01 +0530

    Update sum_of_digits_of_number.c

--- sum_of_digits_of_number.c
+++ sum_of_digits_of_number.c
@@ -9,6 +9,17 @@ Now after extracting the least significant digit, you need the rest of the numbe
 
 For example: 105/10=10 ( / denotes integer division).*/
 
+
+/*The program starts by declaring three variables: n (the input number), a (a variable to store each digit), and s (a variable to store the sum of digits).
+It then asks the user to enter a number using the printf() function and reads the input using the scanf() function.
+The program then enters a while loop that will continue until n becomes 0.
+Inside the while loop, the program uses the modulus operator (%) to find the last digit of n and stores it in the variable a.
+The program then adds the value of a to the variable s.
+The program then divides n by 10 (using the division operator /) to remove the last digit from the number.
+The loop continues until all the digits have been extracted from n and added to s.
+Finally, the program uses printf() function to print the sum of the digits.
+Overall, this program is a simple and effective way to find the sum of digits of a given number in C programming language.*/
+
 #include<stdio.h> 
  
 int main(){ 
