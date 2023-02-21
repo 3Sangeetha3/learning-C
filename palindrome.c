05ca295be352ed36f96007773643aed1c8c7bea4
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2023-02-21 11:59:54 +0530

    palindrome of a number

---
+++ palindrome.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, rev=0, i, originalnum, rem;
+    printf("enter the value of num: ");
+    scanf("%d",&num);
+    originalnum=num;
+    while(num!=0)
+    {
+      rem = num % 10;
+      rev = rev*10+rem;
+      num = num/10;  
+    }
+    
+
+    printf("Reverse number : %d", rev);
+    
+
+    if (originalnum == rev)
+    printf("it is a plaindrome");
+    else
+    printf(" not a palindrome");
+}
