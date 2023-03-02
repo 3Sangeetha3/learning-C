3ff5708adaeceba5bb3cd299ae769971f7f6c770
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2023-03-02 20:03:21 +0530

    Delete unformatted_function.c

--- unformatted_function.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    char C;
-    ///scanf("%c", &c); /// scanf is formmatted function
-    C = getchar();//getchar is the unformatted function
-    ///printf("c = %c",c);
-    putch(C);
-}
