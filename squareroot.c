143a351441b1ae16679fa7906852d92539875fa6
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2023-03-07 22:40:50 +0530

    square root

---
+++ squareroot.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a =2;
+    printf("square root of a is = %f", sqrt(a));
+    printf("\nsquare root of 4 is = %f", sqrt(4));
+
+}
