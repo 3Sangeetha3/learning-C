4511dcdb8166b7577fe2a8fa849412015ae30c40
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2023-03-07 22:38:14 +0530

    math library function

---
+++ math_library_function.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a =-2;
+    float b = -2.5;
+    printf("a = %d, b = %f", a,b);
+    printf("a = %d, b = %f", abs(a),fabs(b));
+
+}
