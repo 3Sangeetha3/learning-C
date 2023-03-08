645a7d740077631c454ec95393696faeacce4cb1
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2023-03-08 15:16:28 +0530

    ceil

---
+++ ceil.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<math.h>
+#include <stdlib.h>
+
+int main()
+{
+    float b = 2.45;
+    printf("\nCeil value of b is : %f",ceil(b));///ceil value is round up like 2.45 = 3 gives upper value
+    printf("\nfloor value of b is : %f",floor(b));///floor value is round down  like 2.45 = 1 gives below value
+    
+
+}
