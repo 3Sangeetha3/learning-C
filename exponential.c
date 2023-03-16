84a3c93ec129ce8d583bdecbbd0e95ff7daab1b6
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2023-03-16 19:00:40 +0530

    exponential

---
+++ exponential.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main()
+{
+    float a=3e2, b=2e4;
+    printf("a = %f b = %f",a,b);
+    
+    float c = 3e-1, d = 2e-2;
+    printf("c = %f d = %f",c,d);
+    return 0;
+}
