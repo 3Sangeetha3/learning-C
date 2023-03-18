511373c7a2d0ca91f8d3e1206861711cb179ee67
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2023-03-18 18:57:21 +0530

    practice.c

---
+++ p1.c
@@ -0,0 +1,37 @@
+/*#include <stdio.h>
+
+int main()
+{
+    printf("hello!");
+    printf("Iam sangeetha");
+    printf("world");
+    printf("\r\r C programming");// '\r' will skip the line and print the next line 
+}*/
+
+/*#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter the value of n: ");
+	scanf("%d",&n);
+	while (n>0){
+		printf("%d\n",n);
+		n--;
+	}
+	return 0;
+}*/
+
+
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter the value of n: ");
+	scanf("%d",&n);
+	while (n<=10){
+		printf("%d\n",n);
+		n++;
+	}
+	return 0;
+}
+
